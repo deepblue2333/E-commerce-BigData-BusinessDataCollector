d12ffae13532eb55ffcb639dd603fd01cc268d64
chengyu <zhouchengyuhun@outlook.com>
2024-08-27 20:27:03 +0800

    success upload to hdfs

---
+++ command/gen_import_config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t activity_info
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t activity_rule 
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t base_category1
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t base_category2
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t base_category3
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t base_dic
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t base_province
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t base_region
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t base_trademark
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t cart_info
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t coupon_info
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t sku_attr_value
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t sku_info
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t sku_sale_attr_value
+python /root/E-commerce-BigData-BusinessDataCollector/script/gen_import_config.py -d gmall -t spu_info
